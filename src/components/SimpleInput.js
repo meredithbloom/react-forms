ad4f80ad6e6f604f8b056565bb904fbc3db6afcb
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-06-10 16:17:02 -0400

    configured submit button disabled, overall form validity, email field added

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -8,6 +8,20 @@ const SimpleInput = (props) => {
 	const enteredNameIsValid = enteredName.trim() !== ''
 	const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
 
+	const [enteredEmail, setEnteredEmail] = useState('')
+	const [enteredEmailTouched, setEnteredEmailTouched] = useState(false)
+
+	const enteredEmailIsValid = enteredEmail.includes('@')
+	const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched
+
+	let formIsValid = false
+
+	if (enteredNameIsValid && enteredEmailIsValid) {
+		formIsValid = true
+	}
+
+
+
 	const nameInputChangeHandler = event => {
 		setEnteredName(event.target.value)
 	}
@@ -16,15 +30,28 @@ const SimpleInput = (props) => {
 		setEnteredNameTouched(true)
 	}
 
+
+	const emailInputChangeHandler = event => {
+		setEnteredEmail(event.target.value)
+	}
+
+	const emailInputBlurHandler = event => {
+		setEnteredEmailTouched(true)
+	}
+
+
 	const formSubmissionHandler = event => {
 		event.preventDefault()
 		setEnteredNameTouched(true)
+		setEnteredEmailTouched(true)
 
-		if (!enteredNameIsValid) {
+		if (!enteredNameIsValid || !enteredEmailIsValid) {
 			return; 
 		}
 		setEnteredName('')
+		setEnteredEmail('')
 		setEnteredNameTouched(false)
+		setEnteredEmailTouched(false)
 		//nameInputRef.current.value = '' => not ideal, don't manipulate the DOM
 	}
 
@@ -43,9 +70,17 @@ const SimpleInput = (props) => {
 					onBlur={nameInputBlurHandler}
 				/>
 				{nameInputIsInvalid && <p className='error-text'>Name must not be empty.</p>}
+			<label htmlFor='email'>Your Email</label>
+				<input
+					type='text'
+					id='email'
+					onChange={emailInputChangeHandler}
+					onBlur={emailInputBlurHandler}
+				/>
+				{emailInputIsInvalid && <p className='error-text'>Correctly formatted email must be entered.</p>}
 		</div>
 		<div className="form-actions">
-			<button>Submit</button>
+			<button disabled={!formIsValid}>Submit</button>
 		</div>
 		</form>
 	);
