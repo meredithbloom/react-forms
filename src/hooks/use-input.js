3cd94847b9ff1d46c9ce750158324417efa3dd44
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-06-10 18:43:04 -0400

    refactored useinput with usereducer

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -11,13 +11,13 @@ const initialInputState = {
 
 const inputStateReducer = (state, action) => {
     if (action.type === 'INPUT') {
-
+        return { value: action.value, isTouched: state.isTouched}
     }
     if (action.type === 'BLUR') {
-        
+        return {isTouched: true, value: state.value}
     }
     if (action.type === 'RESET') {
-        
+        return {isTouched: false, value: ''}
     }
     return inputStateReducer
 }
@@ -26,29 +26,32 @@ const useInput = (validateValue) => {
     const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState)
 
 
-    const [enteredValue, setEnteredValue] = useState('')
-    const [isTouched, setIsTouched] = useState(false)
+    // const [enteredValue, setEnteredValue] = useState('')
+    // const [isTouched, setIsTouched] = useState(false)
 
-    const valueIsValid = validateValue(enteredValue)
-	const hasError = !valueIsValid && isTouched
+    const valueIsValid = validateValue(inputState.value)
+	const hasError = !valueIsValid && inputState.isTouched
 
 
     const valueChangeHandler = event => {
-		setEnteredValue(event.target.value)
+        // setEnteredValue(event.target.value)
+        dispatch({type: 'INPUT', value: event.target.value})
 	}
 
 	const inputBlurHandler = event => {
-		setIsTouched(true)
+		//setIsTouched(true)
+        dispatch({type: 'BLUR'})
     }
     
     const reset = () => {
-        setEnteredValue('')
-        setIsTouched(false)
+        dispatch({type: 'RESET'})
+        // setEnteredValue('')
+        // setIsTouched(false)
     }
 
 
     return {
-        value: enteredValue,
+        value: inputState.value,
         isValid: valueIsValid,
         hasError,
         valueChangeHandler,
